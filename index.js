8626cbc54d8bfbc6dd49f40ffc9b09f8c8375b13
wotanut <75843086+wotanut@users.noreply.github.com>
2021-03-15 13:00:15 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ client.on('ready', () => {
 client.request('SET_ACTIVITY', {
 pid: process.pid,
 activity : {
-details : "Cache, Big things planned",
+details : "Play CRS",
 assets : {
-large_image : "cache",
-large_text : "Invite Cache today!" // THIS WILL SHOW AS "Playing <Status>" from the outisde
+    large_image : "crs",
+    large_text : "Play Today!",
+    small_image: "roblox"
 },
-buttons : [{label : "Join the Support Server" , url : "https://discord.gg/uNKfBdQHUx"},{label : "Invite Cache",url : "https://discord.com/api/oauth2/authorize?client_id=792899384017682473&permissions=8&scope=bot"}]
+buttons : [{label : "Join the Discord Server" , url : "https://discord.gg/ujXp4yq"},{label : "Play The Game",url : "https://www.roblox.com/games/6078379078/Coastial-Railway-Simulator"}]
 }
 })
 })
-client.login({ clientId : "804740685025771572" }).catch(console.error);
+client.login({ clientId : "818219879986888755" }).catch(console.error);
+
